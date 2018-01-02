4302c48f24a3e144816d9b4ef191364d4e27d257
Yash Bonde <bonde.yash97@gmail.com>
2018-01-02 16:20:49 +0530

    added code to core_ops
    Added some functionality to the core_ops still need to be finished and tested, waiting on core to finish

--- Scorpion/scorpion_core_files/scorpion_core_ops.cpp
+++ Scorpion/scorpion_core_files/scorpion_core_ops.cpp
@@ -7,37 +7,104 @@ This source file contain the core operations to be used on Matrix for linear
 algebra operations.
 
 See license for legal queries.
-2017, Yash Bonde
+2017-2018, Yash Bonde
 */
 
-#include "Energia.h" // Energia class to interact with the board
+#include<iostream>
 #include "scorpion_core.h" // Scorpion core to use for making Matrices
 #include "scorpion_core_ops.h" // Scorpion core ops
 
-Matrix core_ops::mat_add(Matrix A, Matrix B){
+//========= Public =========//  
+
+// addition core operation
+static Matrix CoreOps::mat_add(Matrix A, Matrix B){
+	// check for exception
+	shape_check(Matrix A, Matrix b, false, "Addition")
+	// Initializing an empty matrix 
+	std::vector<std::vector<float, B.shape()[1]> > C(A.shape()[0]);
+	for(int i=0; i<A.shape()[0]; i++){
+		//std::vector<float> temp(B.shape()[1]);
+		for(int j=0; j<A.shape()[1]; j++){
+			// addition of elements
+			C[i][j] = A.get_val(i,j) + B.get_val(i,j);
+		}
+		C.push_back(temp);
+	}
+	return Matrix(C);
+}
+
+// subtraction core operation
+static Matrix CoreOps::mat_sub(Matrix A, Matrix B){
+	// check for exception
+	shape_check(Matrix A, Matrix b, false, "Subtraction")
+	// Initializing an empty matrix 
+	std::vector<std::vector<float> > C;
+	for(int i=0; i<A.shape()[0]; i++){
+		std::vector<float> temp(B.shape()[1]);
+		for(int j=0; j<A.shape()[1]; j++){
+			// subtraction of elements
+			temp[j] = A.get_val(i,j) - B.get_val(i,j);
+		}
+		C.push_back(temp);
+	}
+	return Matrix(C);
+}
+
+// element wise multiplication core operation
+static Matrix CoreOps::element_wise_matmul(Matrix A, Matrix B){
+	// check for exception
+	shape_check(Matrix A, Matrix b, false, "Element Wise Multiplication")
+	// Initializing an empty matrix 
+	std::vector<std::vector<float> > C;
+	for(int i=0; i<A.shape()[0]; i++){
+		std::vector<float> temp(B.shape()[1]);
+		for(int j=0; j<A.shape()[1]; j++){
+			// multiplication of elements
+			temp[j] = A.get_val(i,j) * B.get_val(i,j);
+		}
+		C.push_back(temp);
+	}
+	return Matrix(C);
+}
+
+// function to perform matrix multiplication
+static Matrix CoreOps::mat_mul(Matrix A, Matrix B){
+	// check for shape
+	shape_check(Matrix A, Matrix b, true, "MatMul")
+	// rest of the code
+}
+
+// function to divide all the elements of a matrix by a fixed flaot value
+static Matrix CoreOps::divide(Matrix A, float x){
 	// code
 }
 
-Matrix core_ops::mat_sub(Matrix A, Matrix B){
+// function to calculate the determinant of the matrix
+float CoreOps::det(Matrix A){
 	// code
 }
 
-Matrix core_ops::mat_mul(Matrix A, Matrix B){
+// function to find the adjoint of a given matrix
+static Matrix CoreOps::adjoint(Matrix A){
 	// code
 }
 
-Matrix core_ops::element_wise_matmul(Matrix A, Matrix B){
+// function to find the inverse of a given matrix
+static Matrix CoreOps::inverse(Matrix A){
 	// code
 }
 
-float core_ops::det(Matrix A){
-	// code
-}
+//========= Private =========//
 
-Matrix core_ops::adjoint(Matrix A){
-	// code
-}
-
-Matrix core_ops::inverse(Matrix A){
-	// code
+// function to check the shapes of the matrices
+void CoreOps::shape_check(Matrix A, Matrix B, bool mul, std::string op){
+	// if mul = True, check only for ay and bx, for matrix multiplication we need to
+	// verify only these two values
+	int ax = A.shape()[0], ay = A.shape()[1], bx = B.shape()[0], by = B.shape()[1];
+	bool correct = true; // variable to tell if check true or not
+	if (mul == false && (ax != bx || ay != by)) correct = false;
+	if (mul == true && (ay != bx)) correct = false;
+	if not correct{
+		throw std::string("[!] For %s the shapes must be same but got, (%d, %d) and (%d, %d)", op, ax, ay, bx, by);
+	}
 }
--- Scorpion/scorpion_core_files/scorpion_core_ops.h
+++ Scorpion/scorpion_core_files/scorpion_core_ops.h
@@ -7,23 +7,26 @@ This header file contains the core operations for linear algebra. We need to cal
 this file to work on matrix operations.
 
 See license for legal queries.
-2017, Yash Bonde
+2017-2018, Yash Bonde
 */
 
-#ifndef SCORPION_CORE_OPS_H
-#define SCORPION_CORE_OPS_H
+#ifndef SCORPION_CORE_OPS_H_
+#define SCORPION_CORE_OPS_H_
 
+#include "scorpion_core.h" // defining the matrix
 #include "scorpion_core_ops.h" // the name of this header file
 
-class core_ops{
+class CoreOps{
 public:
-	Matrix mat_add(Matrix A, Matrix B); // A+B
-	Matrix mat_sub(Matrix A, Matrix B); // A-B
-	Matrix mat_mul(Matrix A, Matrix B); // A*B -> Matrix multiplication
-	Matrix element_wise_matmul(Matrix A, Matrix B); // A.*B -> Element wise
+	static Matrix mat_add(Matrix A, Matrix B); // A+B
+	static Matrix mat_sub(Matrix A, Matrix B); // A-B
+	static Matrix mat_mul(Matrix A, Matrix B); // A*B -> Matrix multiplication
+	static Matrix element_wise_matmul(Matrix A, Matrix B); // A.*B -> Element wise
 	float det(Matrix A); // |A|
-	Matrix adjoint(Matrix A); // 
-	Matrix inverse(Matrix A);
+	static Matrix adjoint(Matrix A); // find matrix adjoint
+	static Matrix inverse(Matrix A); // find inverse of a given matrix
+private:
+	void shape_check(Matrix A, Matrix B, bool mul, std::string op)
 };
 
 #endif
