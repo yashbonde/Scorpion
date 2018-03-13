b1ece772aff03f29baf1f5479f6db55058d6f50b
Arpit Choudhary <arpitkumar147@gmail.com>
2018-03-13 22:17:42 +0530

    add wrapper

--- Scorpion/scorpion_ops.h
+++ Scorpion/scorpion_ops.h
@@ -9,6 +9,7 @@ scorpion module
 See license for legal queries.
 2017-2018 , Yash Bonde
 */
+#define UNITS 40
 
 class ScorpionOps
 {
@@ -22,12 +23,12 @@ public:
 	 public:
 	 	RNNWrapper(){
 	 		// constructor things go here
-	 		// variables_array so we don't redundantly keep callign them
+	 		// variables_array so we don't redundantly keep calling them
 	 		// other variables,
 	 	};
 	 	~RNNWrapper();
 	 	
-	 	Matrix exectute(); // code to execute the input to the RNNWrapper cell
+	 	Matrix execute(); // code to execute the input to the RNNWrapper cell
 
 	 private:
 	 	// all the information about the RNN wrapper is stored here
@@ -39,6 +40,16 @@ private:
 	// all the information about a single layer is kept here
 };
 
-ScorpionOps::RNNWrapper::execute(Matrix){
-	// 
+ScorpionOps::RNNWrapper::execute(Matrix input, Matrix prev_state){
+	//
+	// I assume input matrix is of dimensions(UNITS, X) and prev_state is of (1, X)
+	int x = input.no_of_cols;
+	Matrix output;
+	vector<Matrix> temp;
+	for (int i = 0; i < UNITS; i++) {
+		temp = lstm_cell(input.get_row(i), , prev_state, ...);
+		prev_state = temp[0];
+		output = temp[1];
+	}
+	return output;
 }
